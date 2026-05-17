7258664293f535ef003fc9b78d8bcb96e1395f35
tomatorivera <tomatorivera@gmail.com>
2026-05-17 18:29:19 -0300

    feat(proyectos): crear estructura principal de página para mostrar proyectos

--- js/app.js
+++
@@ -1,5 +0,0 @@
-const MINIMO_WIDTH_PARA_NAV_ABIERTO = 576; // px
-document.addEventListener('DOMContentLoaded', () => {
-  if (window.screen.width <= MINIMO_WIDTH_PARA_NAV_ABIERTO)
-    document.querySelector('.navbar').classList.add('hide');
-})
--- js/navbar.js
+++ js/navbar.js
@@ -13,3 +13,10 @@ dropdownButtons.forEach(button => {
     button.closest(".dropdown").classList.toggle("dropdown-open");
   });
 });
+
+/* navbar min width */
+const MINIMO_WIDTH_PARA_NAV_ABIERTO = 576; // px
+document.addEventListener('DOMContentLoaded', () => {
+  if (window.screen.width <= MINIMO_WIDTH_PARA_NAV_ABIERTO)
+    document.querySelector('.navbar').classList.add('hide');
+})
